6ddfcd9909f3a784f5f465703cf5eb7785c8bbe1
Emre <ekaraman322@gmail.com>
2018-09-16 23:24:31 +0300

    Deep learning tutorial python codes.

---
+++ tutorial/submit_gpu.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+# 
+# CompecTA (c) 2017
+#
+# You should only work under the /scratch/users/<username> directory.
+#
+# Example job submission script
+#
+# TODO:
+#   - Set name of the job below changing "Test" value.
+#   - Set the requested number of tasks (cpu cores) with --ntasks parameter.
+#   - Select the partition (queue) you want to run the job in:
+#     - short : For jobs that have maximum run time of 120 mins. Has higher priority.
+#     - long  : For jobs that have maximum run time of 7 days. Lower priority than short.
+#     - longer: For testing purposes, queue has 31 days limit but only 3 nodes.
+#   - Set the required time limit for the job with --time parameter.
+#     - Acceptable time formats include "minutes", "minutes:seconds", "hours:minutes:seconds", "days-hours", "days-hours:minutes" and "days-hours:minutes:seconds"
+#   - Put this script and all the input file under the same directory.
+#   - Set the required parameters, input and output file names below.
+#   - If you do not want mail please remove the line that has --mail-type
+#   - Put this script and all the input file under the same directory.
+#   - Submit this file using:
+#      sbatch examle_submit.sh
+
+# -= Resources =-
+#
+#SBATCH --job-name=Test
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --partition=iui
+#SBATCH --time=01:00:00 
+#SBATCH --output=test-%j.out
+#SBATCH --mail-type=ALL
+#SBATCH --gres=gpu:1
+# #SBATCH --mail-user=foo@bar.com
+
+################################################################################
+##################### !!! DO NOT EDIT BELOW THIS LINE !!! ######################
+################################################################################
+
+## Load CUDA 9.1
+echo "CUDA 9.1 loading.."
+module load cuda/9.1
+
+echo ""
+echo "======================================================================================"
+env
+echo "======================================================================================"
+echo ""
+
+# Set stack size to unlimited
+echo "Setting stack size to unlimited..."
+ulimit -s unlimited
+ulimit -l unlimited
+ulimit -a
+echo
+
+echo "Running Example Job...!"
+echo "==============================================================================="
+# Command 1 for matrix
+echo "Running Python script..."
+# Put Python script command below
+python logisticRegression.py 
+
+#echo "Compiling Cuda code"
+#nvcc -o a.out hello_world.cu
+
+#echo "Running CUDA code"
+#./a.out
